94eb28358e4bb8ad7781ade9e33874f77e571af7
gane27 <gane27@gmail.com>
2020-05-04 13:37:00 +0530

    Updates

---
+++ precache-manifest.b43d16cd8d5eaeb02939c379be0bbfa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05d18873c6b39d32c7611cde9b5de79",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "66d3164aff1e066286cd",
+    "url": "/covid19/static/css/main.6cf17792.chunk.css"
+  },
+  {
+    "revision": "348f1c5e4c9c1abc8307",
+    "url": "/covid19/static/js/2.d2e6b6a2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.d2e6b6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d3164aff1e066286cd",
+    "url": "/covid19/static/js/main.5a35abb3.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  },
+  {
+    "revision": "f307723460b5a0472e4194bbc7dec617",
+    "url": "/covid19/static/media/banner.f3077234.png"
+  }
+]);

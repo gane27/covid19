fb111212d944dba61c3d61ae3c491230f9552a76
Ganesh <33153210+gane27@users.noreply.github.com>
2020-05-04 13:46:24 +0530

    Add files via upload

---
+++ precache-manifest.9b9d9a20388a63b2efa75fe68e5c7558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13093a48063052bdd9c3e938840e2923",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "74d902fc8ed21654752d",
+    "url": "/covid19/static/css/main.6cf17792.chunk.css"
+  },
+  {
+    "revision": "348f1c5e4c9c1abc8307",
+    "url": "/covid19/static/js/2.d2e6b6a2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.d2e6b6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d902fc8ed21654752d",
+    "url": "/covid19/static/js/main.94df8f46.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  },
+  {
+    "revision": "f307723460b5a0472e4194bbc7dec617",
+    "url": "/covid19/static/media/banner.f3077234.png"
+  }
+]);
